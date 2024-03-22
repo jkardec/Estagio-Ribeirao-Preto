1103691c85123597b9ad90e6ec02ddc289781968
Jamenson Kardec <47001141+jkardec@users.noreply.github.com>
2024-03-21 21:27:41 -0300

    alteração

--- Estagio Ribeirão Preto Exerc 1/Estagio RIbeirão Preto/Program.cs
+++ Estagio Ribeirão Preto Exerc 1/Estagio RIbeirão Preto/Program.cs
@@ -7,7 +7,7 @@ internal class Program {
         int INDICE = 13, SOMA = 0, K = 0;
 
        /* laço de repetição que incrementa a variável K até que ela seja igual a INDICE = 13,
-        e em cada iteração, adiciona o valor de K à variável SOMA.
+        e em cada iteração, adiciona o valor de K à variável soma.
         Quando K alcança o valor de INDICE, o laço termina e o valor final da SOMA é impresso.*/
 
         while (K < INDICE) {
